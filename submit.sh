dcb3c679dcf8a6d69a785321cdeaeed21971ed99
hu <hujunxianligong@gmail.com>
2014-12-29 17:14:11 +0800

    2.0-beta

---
+++ submit.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+rm *.zip
+rm *.md
+rm -r WebCollector
+
+cp -r /home/hu/mygit/WebCollector/WebCollector ./
+cp -r /home/hu/mygit/WebCollector/*.zip ./
+cp -r /home/hu/mygit/WebCollector/*.md ./
+
+git add --all
+git commit -m '2.0-beta'
+git push
