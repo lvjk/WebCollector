24e52fe011a128a2dab6cf93d0c59364ac1c0fdb
hu <hujunxianligong@gmail.com>
2015-04-22 16:36:57 +0800

    2.05

--- submit.sh
+++ submit.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 rm *.zip
 rm *.md
+rm *.java
 rm -r WebCollector
 rm -r WebCollectorExample
 
@@ -8,6 +9,7 @@ cp -r /home/hu/mygit/WebCollector/WebCollector ./
 cp -r /home/hu/mygit/WebCollector/WebCollectorExample ./
 cp -r /home/hu/mygit/WebCollector/*.zip ./
 cp -r /home/hu/mygit/WebCollector/*.md ./
+cp -r /home/hu/mygit/WebCollector/*.java ./
 
 git add --all
 git commit -m '2.05'
