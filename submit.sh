1282b1e1df8ac2de9a0a99fc0c393acdd9f200fb
hu <hujunxianligong@gmail.com>
2015-12-08 22:34:41 +0800

    2.24

--- submit.sh
+++ submit.sh
@@ -3,15 +3,15 @@ rm *.zip
 rm *.md
 rm *.java
 rm -r WebCollector
-rm -r WebCollectorExample
+rm -r Lazy 
 
 cp -r /home/hu/mygit/WebCollector/WebCollector ./
-cp -r /home/hu/mygit/WebCollector/WebCollectorExample ./
+cp -r /home/hu/mygit/WebCollector/Lazy ./
 cp -r /home/hu/mygit/WebCollector/*.zip ./
 cp -r /home/hu/mygit/WebCollector/*.md ./
 cp -r /home/hu/mygit/WebCollector/*.java ./
 
 
 git add --all
-git commit -m '2.12'
+git commit -m '2.24'
 git push
