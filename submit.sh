9803d84db9bf65039360aef4f4b69d7afa94d93c
hu <hujunxianligong@gmail.com>
2015-12-14 21:22:03 +0800

    webcollector-nutch

--- submit.sh
+++ submit.sh
@@ -3,9 +3,11 @@ rm *.zip
 rm *.md
 rm *.java
 rm -r WebCollector
+rm -r WebCollector-Nutch
 rm -r Lazy 
 
 cp -r /home/hu/mygit/WebCollector/WebCollector ./
+cp -r /home/hu/mygit/WebCollector/WebCollector-Nutch ./
 cp -r /home/hu/mygit/WebCollector/Lazy ./
 cp -r /home/hu/mygit/WebCollector/*.zip ./
 cp -r /home/hu/mygit/WebCollector/*.md ./
@@ -15,5 +17,5 @@ rm -rf Lazy/target
 
 
 git add --all
-git commit -m '2.24'
+git commit -m 'webcollector-nutch'
 git push
